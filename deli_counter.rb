5df37a74ac6b1ae5055d9d842cc42e1715401a35
determined-constant-1339 <curtis.brodie1011@yahoo.com>
2020-06-24 18:03:14 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,38 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    status = "The line is currently:"
+    
+    katz_deli.each_with_index do |name, index| 
+      status += " #{index.to_i+1}. #{name}"
+    end
+    puts "#{status}"
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
+
+
+
+
+
+
+
