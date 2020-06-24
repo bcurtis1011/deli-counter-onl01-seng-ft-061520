f826c49c08176cdef55f513d63092770acafba44
determined-constant-1339 <curtis.brodie1011@yahoo.com>
2020-06-24 17:43:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,10 +8,7 @@ end
 
 
 def take_number(katz_deli, name)
-  katz_deli.push(name)
-  x.each_with_index do |katz_deli, index|
-    puts "Welcome, #{name}. you are number #{index} in line."
-  end
+  
 end
 
 def now_serving
