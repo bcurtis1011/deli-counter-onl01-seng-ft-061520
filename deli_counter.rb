56337e58af6a94d5505004745792cc31609e1d86
determined-constant-1339 <curtis.brodie1011@yahoo.com>
2020-06-24 17:47:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,8 +12,12 @@ def take_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
-  #next perrson in line
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  
+  else
+    puts "Currently serving #{katz_deli.shift}."
 end
 
 
